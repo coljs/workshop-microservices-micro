8e4555d5c121b44dbfab009182c0794ffeec800d
Julian Duque <julianduquej@gmail.com>
2017-04-21 17:15:01 -0400

    separate in individual projects

---
+++ rules.js
@@ -0,0 +1,7 @@
+{
+  "rules": [
+    { "pathname": "/upload", "dest": "medjsmicro-upload.now.sh" },
+    { "pathname": "/preview", "dest": "medjsmicro-preview.now.sh" },
+    { "pathname": "/save", "dest": "medjsmicro-save.now.sh" }
+  ]
+}
